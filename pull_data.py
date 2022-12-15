820af1dc884c5f82c5f9d73aac50d94c865a41e6
Max Schrader <mcschrader@crimson.ua.edu>
2022-12-15 18:48:54 +0000

    Updated model
    Signed-off-by: Max Schrader <mcschrader@crimson.ua.edu>

--- pull_data.py
+++ pull_data.py
@@ -3,6 +3,7 @@ from src.data.download import download_whole_city
 from src.data.make_dataset import add_h3_indices_to_city
 from src.data.load_data import load_filter
 from src.data.make_dataset import group_city_tags
+from src.data.make_dataset import group_cities
 from src.settings import DATA_RAW_DIR
 from tqdm import tqdm
 
@@ -10,19 +11,62 @@ from tqdm import tqdm
 RESOLUTION = 9
 TAG_FILTER = load_filter("from_wiki.json")
 
+# remove tags not of interest
+SELECTED_TAGS = [
+    "aeroway",
+    "amenity",
+    "building",
+    "healthcare",
+    "historic",
+    "landuse",
+    "leisure",
+    "military",
+    "natural",
+    "office",
+    "shop",
+    "sport",
+    "tourism",
+    "water",
+    "waterway",
+]
+
+new_tag_filter = {} 
+for tag in SELECTED_TAGS:
+    new_tag_filter[tag] = TAG_FILTER[tag]
+TAG_FILTER = new_tag_filter.copy()
+    
+
+problem_columns = [
+    'amenity_waste_basket',
+    'landuse_grass',
+    'historic_tomb',
+    'natural_tree',
+    'natural_tree_row',
+    'natural_valley', # northern Warsaw
+]
+
+for tag in problem_columns:
+    super_tag, *sub_tag = tag.split("_")
+    if isinstance(sub_tag, (list, tuple)):
+        sub_tag = "_".join(sub_tag)
+    TAG_FILTER[super_tag] = [
+        tag for tag in TAG_FILTER[super_tag] if tag != sub_tag
+    ]
+
+
 # %%
 cities = [
-    # "Vienna, Austria",
-    # "Minsk, Belarus",
-    # "Brussels, Belgium",
-    # "Sofia, Bulgaria",
-    # "Zagreb, Croatia",
-    # "Prague, Czech Republic",
-    # "Tallinn, Estonia",
-    # "Helsinki, Finland",
-    # "Paris, France",
-    # "Berlin, Germany",
-    # "Reykjavík, Iceland",
+    # "Vienna, Austria",  # I had to remove vienna austria as someone removed the city boundary from OSM
+    "Minsk, Belarus",
+    "Brussels, Belgium",
+    "Sofia, Bulgaria",
+    "Zagreb, Croatia",
+    "Prague, Czech Republic",
+    "Tallinn, Estonia",
+    "Helsinki, Finland",
+    "Paris, France",
+    "Berlin, Germany",
+    "Reykjavík, Iceland",
     "Dublin, Ireland",
     "Rome, Italy",
     "Nur-Sultan, Kazakhstan",
@@ -38,14 +82,14 @@ cities = [
     "Poznań, PL",
     "Gdańsk, PL",
     "Lisbon, Portugal",
-    "Moscow, Russia",
+    ['Moscow, Russia', 'Zelenogradsky Administrative Okrug', 'Western Administrative Okrug', 'Novomoskovsky Administrative Okrug', 'Troitsky Administrative Okrug'],
     "Belgrade, Serbia",
     "Bratislava, Slovakia",
     "Ljubljana, Slovenia",
     "Madrid, Spain",
     "Stockholm, Sweden",
     "Bern, Switzerland",
-    "London, United Kingdom",
+    ["London, United Kingdom", "City of London"],
     "New York City, USA",
     "Chicago, USA",
     "San Francisco, USA",
@@ -80,7 +124,20 @@ if __name__ == "__main__":
     #     t.join()
 
     for city in tqdm(cities):
-        add_h3_indices_to_city(city, RESOLUTION)
+        download_whole_city(city, DATA_RAW_DIR)
+
+    # remove the cities that are lists of cities
+    cities = [city[0] if isinstance(city, (list, tuple)) else city for city in cities]
 
     for city in tqdm(cities):
-        group_city_tags(city, RESOLUTION, filter_values=TAG_FILTER, fill_missing=True)
+        add_h3_indices_to_city(city, RESOLUTION, filter_values=TAG_FILTER)
+
+    for city in tqdm(cities):
+        group_city_tags(city, RESOLUTION, tags=list(TAG_FILTER.keys()), filter_values=TAG_FILTER, fill_missing=True)
+
+    df = group_cities(
+        cities=cities,
+        resolution=RESOLUTION,
+        add_city_column=True,
+    )
+
