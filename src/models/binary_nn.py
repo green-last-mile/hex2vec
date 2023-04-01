2403d65cd4ca3b79a1944ddfe968c596231fef23
Max Schrader <mcschrader@crimson.ua.edu>
2023-04-01 16:42:56 +0000

    geovex implementation
    Signed-off-by: Max Schrader <mcschrader@crimson.ua.edu>

--- src/models/binary_nn.py
+++ src/models/binary_nn.py
@@ -62,3 +62,66 @@ class BinaryNN(pl.LightningModule):
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=0.001)
+
+
+class DistanceNN(pl.LightningModule):
+    def __init__(self, encoder_sizes, max_distance=10):
+        super().__init__()
+
+        self.max_distance = max_distance
+
+        def create_layers(sizes: List[Tuple[int]]) -> nn.Sequential:
+            layers = []
+            for i, (input_size, output_size) in enumerate(sizes):
+                linear = nn.Linear(input_size, output_size)
+                nn.init.xavier_uniform_(linear.weight)
+                layers.append(nn.Linear(input_size, output_size))
+                if i != len(sizes) - 1:
+                    layers.append(nn.ReLU())
+            return nn.Sequential(*layers)
+
+        sizes = list(zip(encoder_sizes[:-1], encoder_sizes[1:]))
+        self.encoder = create_layers(sizes)
+
+        self.distance = nn.Sequential(
+            nn.Linear(encoder_sizes[-1], 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, Xt: torch.Tensor, Xc: torch.Tensor):
+        emb1 = self.encoder(Xt)
+        emb2 = self.encoder(Xc)
+        distance_vector = torch.abs(emb1 - emb2)
+        distance = self.distance(distance_vector)
+        return distance
+        
+    def predict(self, Xt: torch.Tensor, Xc: torch.Tensor):
+        return sigmoid(self(Xt, Xc))
+
+    def custom_weighted_mse_loss(self, y_true, y_pred, weight_factor=1.0):
+        # Apply a weight to the error proportional to the distance
+        y_pred_clamped = torch.clamp(y_pred, 0, self.max_distance)
+        # make the prediction an integer
+
+        weights = torch.pow(y_true, -weight_factor)
+        # Calculate the mean squared error
+        mse_loss = torch.mean(weights * torch.pow(y_true - y_pred, 2))
+        return mse_loss
+
+    def training_step(self, batch, batch_idx):
+        Xt, Xc, y_act, *_ = batch
+        predicted_distance = self(Xt, Xc)
+        loss = self.custom_weighted_mse_loss(y_act, predicted_distance)
+        self.log("train_loss", loss, on_step=True, on_epoch=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        Xt, Xc, y_act, *_ = batch
+        predicted_distance = self(Xt, Xc)
+        loss = self.custom_weighted_mse_loss(y_act, predicted_distance)
+        self.log("validation_loss", loss, on_step=True, on_epoch=True)
+        return loss
+
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
--- src/models/h3_neighbor_dataset.py
+++ src/models/h3_neighbor_dataset.py
@@ -71,3 +71,130 @@ class H3NeighborDataset(Dataset):
         while negative in excluded_indexes:
             negative = np.random.randint(0, len(self.data))
         return negative
+
+    @property
+    def shape(self, ) -> int:
+        return self.data_torch.shape
+
+
+class H3NeighborDatasetCity(H3NeighborDataset):
+    def __init__(self, data: pd.DataFrame, neighbor_k_ring=1, dead_k_ring=2):
+    
+        self.data = data
+
+        if 'city' not in self.data.columns:
+            raise ValueError('Column `city` not found in dataframe. You have to add it before using this dataset, as it groups by city')
+
+
+        self.data_torch = torch.Tensor(self.data.drop(columns=['city']).to_numpy(dtype=np.float32))
+
+        self.inputs = []
+        self.contexts = []
+        self.input_h3 = []
+        self.context_h3 = []
+
+        self.positive_indexes = {}
+
+        for g_idx, g in tqdm(self.data.groupby('city'), total=len(self.data['city'].unique())):
+
+            all_indices = set(g.index)
+
+            for i, (h3_index, hex_data) in tqdm(
+                enumerate(g.iterrows()), total=len(g)
+            ):
+                
+                #  creaet a list of all the neighbors of the current hexagon
+                hex_neighbors_h3 = h3.k_ring(h3_index, neighbor_k_ring)
+                hex_neighbors_h3.remove(h3_index)
+
+                #  remove all the neighbors that are not in the current city
+                available_neighbors_h3 = list(hex_neighbors_h3.intersection(all_indices))
+
+                
+                contexts_indexes = [
+                    self.data.index.get_loc(idx) for idx in available_neighbors_h3
+                ]
+
+                negative_excluded_h3 = h3.k_ring(h3_index, dead_k_ring)
+                negative_excluded_h3 = list(negative_excluded_h3.intersection(all_indices))
+                positive_indexes = [
+                    self.data.index.get_loc(idx) for idx in negative_excluded_h3
+                ]
+
+                self.inputs.extend([i] * len(contexts_indexes))
+                self.contexts.extend(contexts_indexes)
+                self.positive_indexes[h3_index] = set(positive_indexes)
+
+                self.input_h3.extend([h3_index] * len(available_neighbors_h3))
+                self.context_h3.extend(available_neighbors_h3)
+
+        self.inputs = np.array(self.inputs)
+        self.contexts = np.array(self.contexts)
+
+        self.input_h3 = np.array(self.input_h3)
+        self.context_h3 = np.array(self.context_h3)
+
+
+
+
+
+# create dataset loader for the distance prediction task
+class H3DistanceDataset(Dataset):
+
+    def __init__(self, data: pd.DataFrame, max_distance: int = 10) -> None:
+        
+        self.data = data
+        self.data_torch = torch.Tensor(self.data.to_numpy(dtype=np.float32))
+
+        self.index_list = list(self.data.index)
+        
+        self.indice_map = {
+            h3_index: i for i, h3_index in enumerate(self.data.index)
+        }
+
+        # self.neighbor_map = {
+        #     h3_index: h3.k_ring(h3_index, max_distance).intersection(self.data.index) for h3_index in self.data.index
+        # }
+
+        self._k = max_distance
+
+    @property
+    def shape(self) -> int:
+        return self.data_torch.shape
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+
+        h3_target = self.index_list[index]
+        # have a 10% change of getting a neighbor as the target
+        if np.random.random() < 0.1:
+            # h3_d = np.random.choice(list(neighbors))
+            # get the 
+            neighbors = h3.k_ring(h3_target, self._k).intersection(self.index_list)
+            h3_d = np.random.choice(list(neighbors))
+
+        else:
+            # just get a random h3 index
+            h3_d = np.random.choice(self.index_list)
+
+        # return the target, the neighbor, and the distance between them
+        try:
+            distance = min(h3.h3_distance(h3_target, str(h3_d)), 10)
+        except h3.H3ValueError:
+            distance = 10
+        
+        return self.data_torch[index], self.data_torch[self.indice_map[h3_d]], distance 
+
+        # get the index of the h3 index
+
+        
+
+
+
+
+
+            
+
+
