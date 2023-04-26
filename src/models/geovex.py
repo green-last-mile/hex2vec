e00c1325be5b3f09d09b7ebe70a962c2a144c931
Max Schrader <mcschrader@crimson.ua.edu>
2023-04-26 21:01:13 +0000

    added geovex model
    Signed-off-by: Max Schrader <mcschrader@crimson.ua.edu>

--- src/models/geovex.py
+++ src/models/geovex.py
@@ -8,63 +8,187 @@ from torchmetrics.functional import f1_score as f1
 
 
 # TODO! This is incomplete
-
-
 class GeoVeXZIP(nn.Module):
     def __init__(self):
         super(GeoVeXZIP, self).__init__()
 
-    def forward(self, g_pi, g_lambda):
-        pi = torch.sigmoid(g_pi)
-        lambda_ = torch.exp(g_lambda)
-
+    def forward(self, x):
+        pi = torch.sigmoid(x)
+        lambda_ = torch.exp(x)
         return pi, lambda_
 
 
-class GeoVeXDecoder(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(GeoVeXDecoder, self).__init__()
+# class GeoVeXDecoder(nn.Module):
+#     def __init__(self, input_size, output_size):
+#         super(GeoVeXDecoder, self).__init__()
 
-        self.zip_layer = GeoVeXZIP()
-        self.fc_pi = nn.Linear(input_size, output_size)
-        self.fc_lambda = nn.Linear(input_size, output_size)
+#         self.zip_layer = GeoVeXZIP()
+#         self.fc_pi = nn.Linear(input_size, output_size)
+#         self.fc_lambda = nn.Linear(input_size, output_size)
 
-    def forward(self, h):
-        g_pi = self.fc_pi(h)
-        g_lambda = self.fc_lambda(h)
+#     def forward(self, h):
+#         g_pi = self.fc_pi(h)
+#         g_lambda = self.fc_lambda(h)
 
-        pi, lambda_ = self.zip_layer(g_pi, g_lambda)
+#         pi, lambda_ = self.zip_layer(g_pi, g_lambda)
 
-        return pi, lambda_
+#         return pi, lambda_
+
+def build_mask_funcs(R):
+    def w_dist(i, j):
+        r = max(abs(i - R), abs(j - R), abs(i - j))
+        return 1 / (1 + r) if r <= R else 0
     
+    def w_num(i, j):
+        r = max(abs(i - R), abs(j - R), abs(i - j))
+        return 1 / (R * r) if r <= R and r > 0 else 1 if r == 0 else 0
+
+    return w_dist, w_num
 
 class GeoVeXLoss(nn.Module):
     def __init__(self, R):
         super(GeoVeXLoss, self).__init__()
         self.R = R
+        self._w_dist, self._w_num = build_mask_funcs(self.R)
+
+        M = 2 * self.R + 1
+        self._w_dist_matrix = torch.tensor(
+            [[self._w_dist(i, j) for j in range(M)] for i in range(M)], dtype=torch.float32
+        )
+        self._w_num_matrix = torch.tensor(
+            [[self._w_num(i, j) for j in range(M)] for i in range(M)], dtype=torch.float32
+        )
 
     def forward(self, pi, lambda_, y):
         M = 2 * self.R + 1
-        K = pi.size(-1)
-
-        def w_dist(i, j):
-            r = max(abs(i - self.R), abs(j - self.R), abs(i - j))
-            return 1 / (1 + r) if r <= self.R else 0
-
-        def w_num(i, j):
-            r = max(abs(i - self.R), abs(j - self.R), abs(i - j))
-            return 1 / (6 * r) if r <= self.R and r > 0 else 1 if r == 0 else 0
-
-        w_dist_matrix = torch.tensor([[w_dist(i, j) for j in range(M)] for i in range(M)], dtype=torch.float32)
-        w_num_matrix = torch.tensor([[w_num(i, j) for j in range(M)] for i in range(M)], dtype=torch.float32)
+        K = pi.size(1)
 
         I0 = (y == 0).float()
         I_greater_0 = (y > 0).float()
 
         log_likelihood_0 = I0 * torch.log(pi + (1 - pi) * torch.exp(-lambda_))
-        log_likelihood_greater_0 = I_greater_0 * (torch.log(1 - pi) - lambda_ + y * torch.log(lambda_) - torch.lgamma(y + 1))
+        log_likelihood_greater_0 = I_greater_0 * (
+            torch.log(1 - pi) - lambda_ + y * torch.log(lambda_) - torch.lgamma(y + 1)
+        )
 
         log_likelihood = log_likelihood_0 + log_likelihood_greater_0
-        loss = -torch.sum(log_likelihood * w_dist_matrix * w_num_matrix) / (torch.sum(w_dist_matrix) * torch.sum(w_num_matrix))
+        loss = -torch.sum(log_likelihood * self._w_dist_matrix * self._w_num_matrix) / (
+            torch.sum(self._w_dist_matrix) * torch.sum(self._w_num_matrix)
+        )
 
         return loss
+
+
+class HexagonalConv2d(nn.Module):
+    def __init__(
+        self, in_channels, out_channels, kernel_size=3, stride=1, padding=0, bias=True
+    ):
+        super(HexagonalConv2d, self).__init__()
+        self.conv = nn.Conv2d(
+            in_channels, out_channels, kernel_size, stride, padding, bias=bias
+        )
+        self.register_buffer("hexagonal_mask", self.create_hexagonal_mask())
+
+    def create_hexagonal_mask(self):
+        mask = torch.tensor([[0, 1, 1], [1, 1, 1], [1, 1, 0]], dtype=torch.float32)
+        return mask
+
+    def forward(self, x):
+        self.conv.weight = nn.Parameter(self.conv.weight * self.hexagonal_mask)
+        out = self.conv(x)
+        return out
+
+
+class HexagonalConvTranspose2d(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size=3,
+        stride=1,
+        padding=0,
+        output_padding=0,
+        bias=True,
+    ):
+        super(HexagonalConvTranspose2d, self).__init__()
+        self.conv_transpose = nn.ConvTranspose2d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride,
+            padding,
+            output_padding,
+            bias=bias,
+        )
+        self.register_buffer("hexagonal_mask", self.create_hexagonal_mask())
+
+    def create_hexagonal_mask(self):
+        mask = torch.tensor([[0, 1, 1], [1, 1, 1], [1, 1, 0]], dtype=torch.float32)
+        return mask
+
+    def forward(self, x):
+        self.conv_transpose.weight.data *= self.hexagonal_mask
+        out = self.conv_transpose(x)
+        return out
+
+
+class Reshape(nn.Module):
+    def __init__(self, shape):
+        super(Reshape, self).__init__()
+        self.shape = shape
+
+    def forward(self, x):
+        return x.view(self.shape)
+
+
+class GeoVexModel(pl.LightningModule):
+    def __init__(self, k_dim, R, lr=1e-3, weight_decay=1e-5):
+        super().__init__()
+
+        self.k_dim = k_dim
+        self.R = R
+        self.lr = lr
+
+        self.encoder = nn.Sequential(
+            nn.BatchNorm2d(self.k_dim),
+            nn.ReLU(),
+            HexagonalConv2d(self.k_dim, 256, kernel_size=3),
+            # HexagonalConv2d(512, 256, kernel_size=3),
+            HexagonalConv2d(256, 128, kernel_size=3),
+            nn.Flatten(),
+            # TODO: make this a function of R
+            nn.Linear(5 * 5 * 128, 32),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(32, 5 * 5 * 128),
+            # maintain the batch size, but reshape the rest
+            Reshape((-1, 128, 5, 5)),
+            HexagonalConvTranspose2d(128, 256, kernel_size=3),
+            HexagonalConvTranspose2d(256, self.k_dim, kernel_size=3),
+            GeoVeXZIP(),
+        )
+
+        self._loss = GeoVeXLoss(self.R)
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+
+    def training_step(self, batch, batch_idx):
+        x = batch
+        pi, lambda_ = self(x)
+        loss = self._loss.forward(pi, lambda_, x)
+        self.log("train_loss", loss, on_step=True, on_epoch=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x  = batch
+        pi, lambda_ = self(x)
+        loss = self._loss.forward(pi, lambda_, x)
+        self.log("validation_loss", loss, on_step=True, on_epoch=True)
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.0001)
---
+++ src/models/geovex_loader.py
@@ -0,0 +1,95 @@
+import h3
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+from tqdm import tqdm
+
+
+class GeoVexLoader(Dataset):
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        neighbor_k_ring=6,
+    ):
+        # todo: find all h3's that have all of their neighbors in the dataset,
+        # OR, how do we handle h3's that don't have all of their neighbors in the dataset?
+        self._valid_h3 = []
+        all_indices = set(data.index)
+
+        # number of columns in the dataset
+        self._N = data.shape[1]
+        self._k = neighbor_k_ring
+
+        for i, (h3_index, hex_data) in tqdm(
+            enumerate(data.iterrows()), total=len(data)
+        ):
+            hex_neighbors_h3 = h3.grid_disk(h3_index, neighbor_k_ring)
+            # remove the h3_index from the neighbors
+            hex_neighbors_h3.remove(h3_index)
+            available_neighbors_h3 = list(hex_neighbors_h3.intersection(all_indices))
+            if len(available_neighbors_h3) < len(hex_neighbors_h3):
+                # skip adding this h3 as a valid input
+                continue
+            anchor = np.array(h3.cell_to_local_ij(h3_index, h3_index))
+            self._valid_h3.append(
+                (
+                    data.index.get_loc(h3_index),
+                    [
+                        # get the index of the h3 in the dataset
+                        (
+                            data.index.get_loc(_h),
+                            tuple(
+                                (
+                                    np.array(h3.cell_to_local_ij(h3_index, _h)) - anchor
+                                ).tolist()
+                            ),
+                        )
+                        for _h in hex_neighbors_h3
+                    ],
+                )
+            )
+
+        self._data = data.to_numpy(dtype=np.float32)
+        self._data_torch = torch.Tensor(self._data)
+
+    def __len__(self):
+        return len(self._valid_h3)
+
+    def __getitem__(self, index):
+        # construct the 3d tensor
+        target_idx, neighbors_idxs = self._valid_h3[index]
+        # build the 3d tensor
+        # it is a tensor with diagonals of length neighbor_k_ring
+        # the diagonals are the neighbors of the target h3
+        # the target h3 is in the center of the tensor
+        # the tensor is 2*neighbor_k_ring + 1 x 2*neighbor_k_ring + 1 x 2*neighbor_k_ring + 1
+        # make a tensor of zeros
+        tensor = torch.zeros(
+            (
+                self._N,
+                2 * self._k + 1,
+                2 * self._k + 1,
+            )
+        )
+
+        # set the target h3 to the center of the tensor
+        tensor[
+            :,
+            self._k,
+            self._k,
+        ] = self._data_torch[target_idx]
+
+        # set the neighbors of the target h3 to the diagonals of the tensor
+        for neighbor_idx, (i, j) in neighbors_idxs:
+            tensor[:, self._k + i, self._k + j] = self._data_torch[neighbor_idx]
+
+        # return the tensor and the target (which is same as the tensor)
+        # should we return the target as a copy of the tensor?
+        return tensor
+
+    @property
+    def shape(
+        self,
+    ) -> int:
+        return self._N, (2 * self._k + 1), (2 * self._k + 1)
